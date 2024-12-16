2020361b3975e7203761990506891aedf2d47c4f
Tanvir-Chowdhury <tanvirvlogger@gmail.com>
2024-12-16 14:23:32 +0600

    Complete image viewer

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <QApplication>
 #include <QLabel>
 #include <QPixmap>
@@ -111,36 +110,3 @@ int main(int argc, char* argv[]) {
 }
 
 #include "main.moc"
-=======
-#include "nocs.h"
-
-int main()
-{
-    NocList nocsList;
-
-    // Load data from the CSV file
-    string filePath = "data/nocs.csv";
-    nocsList.loadFromCSV(filePath);
-
-    // Print the linked list
-    //nocsList.printList();
-
-    // Append a new row to the CSV and linked list
-    //cout << "\nAdding a new entry to the CSV and linked list..." << endl;
-    //nocsList.appendToCSVAndList(filePath, "NEW", "Newland", "Newlandia", "TestTag", "New note");
-
-    // Print the updated linked list
-    //cout << "\nUpdated Linked List Contents:" << endl;
-    //nocsList.printList();
-
-    // Delete a row from the CSV and linked list
-    cout << "\nDeleting the entry with code 'NEW'..." << endl;
-    nocsList.deleteFromCSVAndList(filePath, "NEW");
-
-    // Print the linked list after deletion
-    cout << "\nLinked List Contents After Deletion:" << endl;
-    nocsList.printList();
-
-    return 0;
-}
->>>>>>> f8e8f257ada51c41ef6ef318667ca07c49a6bbd2
