50637ed08bcd6fbede8950ca2459d122768c37ea
Sujat Ahmed <sujatahmed842002@gmail.com>
2024-12-20 21:03:51 +0600

    push

--- main.cpp
+++ main.cpp
@@ -7,20 +7,15 @@
 #include "QWidget"
 #include <qwidget.h>
 #include <string>
-#include <vector>
 
 using namespace std;
 int main(int argc, char *argv[]) {
-  std::string dataFolder = "/home/sujat/projects/CSE225-summer24-project/data1";
-  QString csvFileName = "athletes.csv"; // Update with the name of your CSV file
-  //
-  //
+  std::string dataFolder = "/home/sujat/projects/CSE225-summer24-project/data";
+
   Database *db = new Database();
   db->loadDatabase(dataFolder);
   cout << "works" << endl;
 
   MenuHandler menu(db, dataFolder);
   menu.start();
-
-
 }
